5bee230a4f02b0e3ee0522a82fc20346fe3a2196
Clement Teboul <teboul.clement@gmail.com>
2018-02-27 15:46:32 +0100

    add canvas

--- src/logger.js
+++
@@ -1,3 +0,0 @@
-export default function () {
-  console.log('Logger.js call 2');
-}
--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
-import test from './logger';
+import $ from 'jquery';
 
-test();
+const CANVAS = $('#game');
+const CANVAS_WIDTH = CANVAS.parent().width();
+const CANVAS_HEIGHT = CANVAS.parent().height();
+
+CANVAS.attr('width', CANVAS_WIDTH);
+CANVAS.attr('height', CANVAS_HEIGHT);
