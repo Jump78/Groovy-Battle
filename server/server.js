c3a7f3c39572ce26e308394aad35453c25ea1f20
Clement Teboul <teboul.clement@gmail.com>
2018-04-06 11:31:21 +0200

    add Server : Push new event in array. Deal with each element each 10 ms

--- server/server.js
+++ server/server.js
@@ -2,14 +2,50 @@ const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+
+let players_lst = [];
+
+let eventList = [];
 
 io.on('connection', function(socket){
   console.log('a user connected');
+
+  if (players_lst.length > 2) return;
+
+  if (players_lst.indexOf('disconnected') >= 0) {
+    players_lst[players_lst.indexOf('disconnected')] = socket.id;
+  } else {
+    players_lst.push(socket.id);
+  }
+
+  socket.on('newAction', function (data) {
+    console.log((players_lst.indexOf(socket.id) + 1) +' as', data.type, data.spell);
+
+    data.player = players_lst.indexOf(socket.id) + 1;
+
+    eventList.push(data);
+  });
+
+  socket.on('disconnect', data => {
+    players_lst[players_lst.indexOf(socket.id)] = 'disconnected';
+  })
+
+  socket.emit('connectionAllowed', {id: players_lst.indexOf(socket.id) + 1});
 });
+
+function eventDeal() {
+  const eventToDeal =  eventList.shift();
+  if (!eventToDeal) return;
+
+  if (eventToDeal.player == 1) {
+    io.sockets.to(players_lst[1]).emit('newEvent', eventToDeal);
+  } else if (eventToDeal.player == 2){
+    io.sockets.to(players_lst[0]).emit('newEvent', eventToDeal);
+  }
+}
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
+
+  setInterval(eventDeal, 10);
 });
