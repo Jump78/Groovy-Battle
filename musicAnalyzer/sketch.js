2cd12864fee320dbda0197e27005855a3a70f1eb
Jump <teboul.clement@gmail.com>
2021-04-04 21:45:32 +0200

    Revert "Revert "update music analyzer: Add minimal offset between 2 notes""
    This reverts commit 2793a713b23adfc77bcd615cdc14d5809c598a70.

--- musicAnalyzer/sketch.js
+++ musicAnalyzer/sketch.js
@@ -7,15 +7,17 @@ let treble = 0;
 let mySound;
 const directions = ['up', 'left', 'down', 'left'];
 let arrows = [];
+let lastNoteTimestamp = 0;
+let offsetNote = 0.150;
 
 function setup() {
-  amplitude = new p5.Amplitude();
-  fft = new p5.FFT();
-
   const input = document.getElementById('input');
 
   input.addEventListener('change', function(event){
     event.preventDefault();
+
+    amplitude = new p5.Amplitude();
+    fft = new p5.FFT();
 
     let sound = URL.createObjectURL(this.files[0]);
     sound.onend = function(e) {
@@ -40,6 +42,15 @@ function setup() {
 }
 
 function draw() {
+  if (!mySound) {
+    return;
+  }
+
+  if (lastNoteTimestamp > 0 && mySound.currentTime() - lastNoteTimestamp < offsetNote) {
+    return;
+  }
+  lastNoteTimestamp = mySound.currentTime();
+
   fft.analyze();
 
   let currentBass = fft.getEnergy("bass");
